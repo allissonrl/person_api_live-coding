db93cdc29e3e132e29dc1e5e1f2b005408fc0574
allissonrl <rose.allisson@gmail.com>
2021-07-11 23:12:09 -0300

    primeiro commit

---
+++ src/test/java/one/digitalinovation/personaApi/PersonaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinovation.personaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
